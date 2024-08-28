6d6cd1008e123da55fbce4f5922c6ac016095ba7
halkman <halk-man@mail.ru>
2024-08-28 13:17:58 +0500

    Нолики и крестики

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -44,30 +44,22 @@ namespace WindowsFormsApp1
             pct.MouseClick += pctLineXY_MouseClick; // подписка на клик или что то в этом роде
 
             Graphics g = pctLineXY.CreateGraphics();
-            //Graphics r = pctLineXY.CreateGraphics();
             Pen pn = new Pen(Color.Black, 2);
 
             for (int i = 0; i <= countW; i++)
             {
                 g.DrawLine(pn, buffposX, buffposY, buffposX, height);
-                //g.DrawEllipse(pn, buffposX, buffposY, 25, 25); //Рисуем кружки в строчку по горизонтали
                 buffposX += stepW;
                 
             }
             for (int i = 0; i <= countH; i++)
             {
                 g.DrawLine(pn, buffposNull, buffposY1, width, buffposX1);
-                // g.DrawEllipse(pn, buffposNull, buffposY1, 25, 25); //Рисуем кружки в строчку по вертикали
                 buffposY1 += stepH;
                 buffposX1 += stepH;
                 
             }
-           // g.DrawEllipse(pn, buffposX, buffposY1, width, buffposX1);
-            //g.DrawEllipse(pn, 20, 150,30,30);
-            //if (m.Button ==  MouseButtons.Left)
-            //{
-            //    g.DrawEllipse(pn, m.X, m.Y, 20, 20);
-            // g.DrawEllipse(pn, buffposX, buffposY1, width, height);
+       
         }
 
         private void lblHelloWorld_Click(object sender, EventArgs e)
@@ -89,22 +81,81 @@ namespace WindowsFormsApp1
         {
 
         }
+        public void CentrovkaNuLLika(MouseEventArgs e) //пока не реализован 
+        {
+            int width = pctLineXY.Width;
+            int height = pctLineXY.Height;
+            int stepx = width / 10; //ширина ячейки 
+            int stepy = height / 10;// высота ячейки
+            int bufX = e.X / stepx; //количество целых ячеек
+            int bufY = e.Y / stepy;
+            int coordinataX = bufX * stepx + (stepx / 2);
+            int coordinataY = bufY * stepy + (stepy / 2);
+            Graphics g = pctLineXY.CreateGraphics();
+            Pen pn = new Pen(Color.Red, 3);
+            g.DrawEllipse(pn, coordinataX - 17, coordinataY - 17, 34, 34);
+            //return coordinataX;
+            //return coordinataY;
+        }
+        public void CentrovkaKrestika(MouseEventArgs e) //пока не реализован 
+        {
+            int width = pctLineXY.Width;
+            int height = pctLineXY.Height;
+            int stepx = width / 10; //ширина ячейки 
+            int stepy = height / 10;// высота ячейки
+            int bufX = e.X / stepx; //количество целых ячеек
+            int bufY = e.Y / stepy;
+
+            int coordinataX1 = bufX * stepx;//верхняя левая
+            int coordinataY1 = bufY * stepy;
+           
+            int coordinataX2 = bufX * stepx + stepx;//верхняя правая
+            int coordinataY2 = bufY * stepy;
+            
+            int coordinataX3 = bufX * stepx;//верхняя правая
+            int coordinataY3 = bufY * stepy + stepy;
+
+            int coordinataX4 = bufX * stepx + stepx;//нижняя правая
+            int coordinataY4 = bufY * stepy + stepy;
+
+
+            Graphics g = pctLineXY.CreateGraphics();
+            Pen pn = new Pen(Color.Blue, 3);
+            g.DrawLine(pn, coordinataX1 , coordinataY1 , coordinataX4, coordinataY4);
+            g.DrawLine(pn, coordinataX3, coordinataY3 , coordinataX2, coordinataY2);
+            //return coordinataX;
+            //return coordinataY;
+        }
 
         private void pctLineXY_MouseClick(object sender, MouseEventArgs e) // событие клика левой клавишей по пикчербоксу
         {
 
-            //var location = e.Location; //пытаюсь получить координаты положения по клику 
-            //var mX = location.X;
-            //var mY = location.Y;
-
-            //var pic = (btnClickThis_Click)sender;
-            Graphics g = pctLineXY.CreateGraphics();
-            Pen pn = new Pen(Color.Red, 2);
+           
+            if (e.Button == MouseButtons.Right)
+            {
+                CentrovkaKrestika(e);
+                //int width = pctLineXY.Width;
+                //int height = pctLineXY.Height;
+                //int stepx = width / 10; //ширина ячейки 
+                //int stepy = height / 10;// высота ячейки
+                //int bufX = e.X / stepx; //количество целых ячеек
+                //int bufY = e.Y / stepy;
+                //int coordinataX = bufX * stepx + (stepx / 2);
+                //int coordinataY = bufY * stepy + (stepy / 2);
+               
+                //Graphics g = pctLineXY.CreateGraphics();
+                //Pen pn = new Pen(Color.Green, 2);
+                //g.DrawEllipse(pn, coordinataX-10, coordinataY-10, 20, 20);
+            }
             
             if (e.Button == MouseButtons.Left)
             {
-                g.DrawEllipse(pn, e.X-15, e.Y-15, 30, 30);
-                
+                CentrovkaNuLLika(e);
+               // CentrovkaKrestik(e);
+                //Graphics g = pctLineXY.CreateGraphics();
+                //Pen pn = new Pen(Color.Red, 2);
+                //g.DrawEllipse(pn, e.X-15, e.Y-15, 30, 30);
+
             }
         }
     }
